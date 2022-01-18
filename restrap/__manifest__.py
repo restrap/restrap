d418bdfc5185778d9e33486607352ea4e591ad00
ahmed3baid <ahmed3baid@live.com>
2022-01-18 20:36:10 +0400

    MAN17 - Splitting Manufacturing Order

---
+++ restrap/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Restrap Module",
+    "version": "15.0.1",
+    "author": "Smart IT Ltd",
+    "category": "Other",
+    "summary": "Zelus",
+    'description': "Custom module for Restrap developed by Smart IT Ltd",
+    'maintainer': "Smart IT Ltd",
+    'website': 'smart-ltd.co.uk',
+    "depends": ['base', 'mrp_workorder'],
+    "data": [
+        'security/mrp_security.xml',
+        'views/res_config_settings_view.xml',
+        'views/mrp_production_views.xml',
+        'views/mrp_workorder_views.xml',
+    ],
+}
