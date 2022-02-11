ed1c742b2ad3609a59930ce54ea6265f9820e739
jonny0126 <jonathan.marchant@smart-ltd.co.uk>
2022-02-11 15:02:40 +0000

    remove

--- restrap/models/mrp_production.py
+++ restrap/models/mrp_production.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from collections import defaultdict
-from curses.ascii import US
 import datetime
 
 from odoo import fields, models, _
