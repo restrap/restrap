b68a2521aeadd8fc0c4a6de05402d9ea67611da5
ahmed3baid <ahmed3baid@live.com>
2022-03-18 11:37:33 +0400

    Add restrap plan model to use it to have all related WOs with the same color

--- restrap/models/mrp_workorder.py
+++ restrap/models/mrp_workorder.py
@@ -7,4 +7,4 @@ from odoo import fields, models, _
 class MrpWorkOrder(models.Model):
     _inherit = 'mrp.workorder'
 
-    split_order_id = fields.Many2one('mrp.production', string="Split Order", help="A field that is used to show related MOs with the same color in work order planning Gantt view.")
+    restrap_plan_id = fields.Many2one('restrap.mrp.plan', string="Restrap Plan", help="A field that is used to show related MOs with the same color in work order planning Gantt view.")
