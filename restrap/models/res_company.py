87c09c72a544c098b5870e4fc5a4d8a7ec3df605
mhornesit <47520837+mhornesit@users.noreply.github.com>
2022-05-20 10:45:06 +0100

    Merge pull request #27 from Smart-IT-Kirkham/RES-CR26460
    Res cr26460

--- restrap/models/res_company.py
+++ restrap/models/res_company.py
@@ -9,3 +9,10 @@ class ResCompany(models.Model):
 
     mrp_split = fields.Boolean("Manufacture Order Split")
     mrp_split_duration = fields.Float("Manufacture Order Split Duration")
+    eori = fields.Char(string="EORI")
+
+
+class BaseDocumentLayout(models.TransientModel):
+    _inherit = 'base.document.layout'
+
+    eori = fields.Char(string="EORI", related='company_id.eori')
