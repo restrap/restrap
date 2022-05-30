fa84594ad3c116ef95fbeac8232aa8a41d4fc021
Dan-SmartIT <dan.wareing@smart-ltd.co.uk>
2022-05-30 17:03:28 +0100

    Contact error message fix

--- restrap/models/res_partner.py
+++ restrap/models/res_partner.py
@@ -1,9 +1,13 @@
 from odoo import fields, models, api
 
+
 class ResPartner(models.Model):
-    _name = "res.partner"
     _inherit = "res.partner"
 
+    @api.model_create_multi
     def create(self, vals_list):
-        vals_list['company_id'] = self.env.company.id
+        # BUG for ticket 28510.
+        # FIX: can save new contacts.
+        for vals in vals_list:
+            vals['company_id'] = self.env.company.id
         return super(ResPartner, self).create(vals_list)
