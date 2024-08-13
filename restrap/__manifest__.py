efb3a42297d7c53a467d749f1fe314034e054d6b
pragmatic <pragmatic>
2024-08-13 13:19:06 +0530

    update the code

--- restrap/__manifest__.py
+++ restrap/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Restrap Module",
-    "version": "17.0.1.2",
+    "version": "17.0.1.3",
     "author": "Smart IT Ltd",
     "category": "Other",
     "summary": "Restrap Smart IT Developments",
