8f418822597bef6ae4210986b46ede7d2d32f00e
pragmatic <pragmatic>
2024-10-09 18:54:02 +0530

    update

--- website_custom/__manifest__.py
+++ website_custom/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Website Custom',
-    'version': '17.7',
+    'version': '17.8',
     'author': 'Pragmatic TechSoft Pvt Ltd.',
     'website': 'http://www.pragtech.co.in',
     'category': 'Website',
